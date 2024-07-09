71bb0ddd0b2f44aa20026665fa31ffe5fa25960c
Stéphanie Parcollet <st.parcollet@orange.fr>
2024-07-10 01:56:26 +0200

    spider et pipeline vériéfiés pour les premières pages de scraping

--- formation_simplon/formation_simplon/pipelines.py
+++ formation_simplon/formation_simplon/pipelines.py
@@ -1,13 +1,50 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import re
 
 class FormationSimplonPipeline:
     def process_item(self, item, spider):
+        item = self.clean_categorie(item)
+        item = self.clean_rncp(item)
+        item = self.clean_rs(item)
+        item = self.clean_voie_acces(item)
+
+        return item
+    
+    def clean_categorie(self, item):
+        adapter = ItemAdapter(item)
+        categorie = adapter.get("categorie")
+        if categorie:
+            categorie = categorie.strip()
+        adapter["categorie"] = categorie
+        return item
+    
+    def clean_rncp(self, item):
+        adapter = ItemAdapter(item)
+        rncp = adapter.get("code_rncp")
+        if rncp is not None:
+            if "RNCP" in rncp:
+                rncp = re.findall(r'(\d+)', rncp)[0]
+            else:
+                rncp = None
+            adapter["code_rncp"] = rncp
+        return item
+    
+    def clean_rs(self, item):
+        adapter = ItemAdapter(item)
+        rs = adapter.get("code_rs")
+        if rs is not None:
+            if "RS" in rs:
+                rs = re.findall(r'(\d+)', rs)[0]
+            else:
+                rs = None
+            adapter["code_rs"] = rs
+        return item
+    
+    def clean_voie_acces(self, item):
+        adapter = ItemAdapter(item)
+        voie_acces = adapter.get("voie_acces")
+        for i in range(len(voie_acces)):
+            voie_acces[i] = voie_acces[i].replace(";","")
+            voie_acces[i] = voie_acces[i].strip()
+        adapter["voie_acces"] = voie_acces
         return item
--- formation_simplon/formation_simplon/spiders/simplon_spider.py
+++ formation_simplon/formation_simplon/spiders/simplon_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-from simplon_formation.items import FormationSimplonItem
+from formation_simplon.items import FormationSimplonItem
 
 class SimplonSpiderSpider(scrapy.Spider):
     name = "simplon_spider"
@@ -10,7 +10,7 @@ class SimplonSpiderSpider(scrapy.Spider):
         formations = response.xpath("//a[@class='btn btn-pricipale btn-formation' and contains(text(), 'Découvrez la formation')]")
         for formation in formations:
             formation_url = formation.xpath("./@href").get()
-            yield response.follow(formation_url, callback=self.parse_formation)
+            yield response.follow(formation, callback=self.parse_formation)
 
     def parse_formation(self, response):
         item = FormationSimplonItem()
@@ -18,35 +18,60 @@ class SimplonSpiderSpider(scrapy.Spider):
         item["categorie"] = response.xpath("//ol/li/a/text()").getall()[-1]
         item["code_rncp"] = response.xpath("//a[contains(@href, 'https://www.francecompetences.fr/recherche/rncp/')]/text()").get()
         item["code_rs"] = response.xpath("//a[contains(@href, 'https://www.francecompetences.fr/recherche/rs/')]/text()").get()
-        item["voie_acces"] = response.xpath("//h3[contains(text(), 'Voie d’accès à la certification')]/following-sibling::ul/li/text()").getall()
-        session_page = response.xpath("//a[contains(text(),'Les sessions ouvertes')]/@href").get()
+        item["voie_acces"] = response.xpath("//h3[contains(text(), 'Voie d’accès à la certification')]/following-sibling::ul/li/text()|//h3[contains(text(), 'Voie d’accès à la certification')]/following-sibling::ul/li/p/text()").getall()
+        
+        # test parse_formation seul
+        yield item
+
+
+        # session_page = response.xpath("//a[contains(text(),'Les sessions ouvertes')]/@href").get()
         # if session_page is not None:
-        #     yield response.follow(session_page, callback=self.parse_session)
+        #     yield response.follow(session_page, meta={"item":item}, callback=self.parse_page_sessions)
         # else:
-        #     yield item
-        yield item
+        #     if item["code_rncp"] is not None:
+        #         rncp_url = response.xpath("//a[contains(@href, 'https://www.francecompetences.fr/recherche/rncp/')]/@href").get()
+        #         yield scrapy.Request(rncp_url, meta={"item":item}, callback=self.parse_registre)
+        #     else:
+        #         yield item
 
+    # def parse_page_sessions(self, response):
+    #     item = response.meta["item"]
+    #     sessions = response.xpath("//a[contains(text(),'Découvrez la session')]")
+    #     for session in sessions:
+    #         session_item = FormationSimplonItem(item)
+    #         session_url = session.xpath("./@href").get()
+    #         if session_url is not None:
+    #             yield response.follow(session_url, meta={"item":session_item}, callback=self.parse_session)
+    #         else:
+    #             yield item
 
-    def parse_session(self, response):
-        item = FormationSimplonItem()    
-        item["date_limite"] = response.xpath("//div[@class='date-bloc']/div/text()").getall()
-        item["agence"] = response.xpath("//div[@class='card-session-info']/text()").getall()[1]
-        item["distanciel"] = response.xpath("//strong[contains(text(), 'alternance')]").get()
-        item["alternance"] = response.xpath("//strong[contains(text(), 'alternance')]").get()
-        item["echelle_duree"] = response.xpath("//div[@class='card-session-info']/text()").getall()[0]
-        item["date_debut"] = response.xpath("//div[@class='card-session-info calendar']/text()").get()
-        item["date_fin"] = response.xpath("//strong[contains(text(), 'Dates de la formation')]").get()
+    # def parse_session(self, response):
+    #     item = response.meta["item"] 
+    #     # item = FormationSimplonItem()   
+    #     item["date_limite"] = response.xpath("//div[@class='date-bloc']/div/text()").getall()
+    #     item["agence"] = response.xpath("//i[contains(text(), 'location_on')]/parent::div/text()").getall()
+    #     item["distanciel"] = response.xpath("//strong[contains(text(), '100%')]/text()").get()
+    #     item["alternance"] = response.xpath("//strong[contains(text(), 'alternance')]/text()").get()
+    #     item["echelle_duree"] = response.xpath("//i[contains(text(), 'hourglass_empty')]/parent::div/text()").getall()
+    #     item["date_debut"] = response.xpath("//div[@class='card-session-info calendar']/text()").getall()
+    #     item["date_fin"] = response.xpath("//strong[contains(text(), 'Dates de la formation')]/text()").get()
+    #     rncp_url =  response.xpath("//a[contains(@href, 'https://www.francecompetences.fr/recherche/rncp/')]/@href").get()
+        
+    #     # yield item
+        
+    #     if rncp_url is not None:
+    #         yield scrapy.Request(rncp_url, meta={"item":item}, callback=self.parse_registre)
+    #     else:
+    #         yield item
+ 
+    # def parse_registre(self, response):
+    #     item = response.meta["item"]
+    #     item["titre"] = response.xpath("//h1/text()").get()
+    #     item["etat_registre"] = response.xpath("//span[contains(text(), 'Etat')]/following-sibling::span/text()").get()
+    #     item["formacode_code"] = response.xpath("//p[contains(text(),'Formacode')]/following-sibling::div/p/span/text()").getall()
+    #     item["formacode_nom"] = response.xpath("//p[contains(text(),'Formacode')]/following-sibling::div/p/text()").getall()
+    #     item["niveau_sortie"] = response.xpath("//p[contains(text(),'Nomenclature')]/following-sibling::div//span/text()").get()
+    #     item["certificateur_nom"] = response.xpath("//button[contains(text(),'Certificateur(s)')]/parent::div//td/text()").getall()[0]
+    #     item["siret"] = response.xpath("//button[contains(text(),'Certificateur(s)')]/parent::div//td/text()").getall()[1]
 
-        yield item
-
-    def parse_registre(self, response):
-        item = FormationSimplonItem()
-        item["titre"] = response.xpath("//h1/text()").get()
-        item["etat_registre"] = response.xpath("//span[contains(text(), 'Etat')]/following-sibling::span/text()").get()
-        item["formacode_code"] = response.xpath("//p[contains(text(),'Formacode')]/following-sibling::div/p/span/text()").getall()
-        item["formacode_nom"] = response.xpath("//p[contains(text(),'Formacode')]/following-sibling::div/p/text()").getall()
-        item["niveau_sortie"] = response.xpath("//p[contains(text(),'Nomenclature')]/following-sibling::div//span/text()").get()
-        item["certificateur_nom"] = response.xpath("//button[contains(text(),'Certificateur(s)')]/parent::div//td/text()").getall()[0]
-        item["siret"] = response.xpath("//button[contains(text(),'Certificateur(s)')]/parent::div//td/text()").getall()[1]
-
-        yield item
+    #     yield item
