5e5436a9e5cd9bf8529afb8faa0df607b9135550
Gwhite <guilhem.dolet@gmail.com>
2024-07-18 15:20:13 +0200

    models.py utilisé : guilhem. Chargement des Tables FormationsExt + Regions (et leur table d'association) OK

--- formation_simplon/formation_simplon/import_bdd.py
+++ formation_simplon/formation_simplon/import_bdd.py
@@ -1,5 +1,6 @@
-from models import Session, FormationsExt
+from model_bdd.models import Session, FormationsExt, Regions
 import json
+
 
 def load_json_into_databse(Session):
 
@@ -9,11 +10,26 @@ def load_json_into_databse(Session):
     session = Session()
     try:
         for entry in data:
-            une_ligne = FormationsExt(
+            # Remplissage de la table FormationExt
+            ligne_formation_ext = FormationsExt(
                 intitule_formation = entry.get('intitule_certification'),
                 organisme = entry.get('nom_of')
             )
-            session.add(une_ligne)
+            session.add(ligne_formation_ext)
+
+            #Remplissage de la table Region
+            region_exist = session.query(Regions).filter_by(region=entry.get('nom_region')).first()
+            
+            if region_exist is None and entry.get('nom_region'):
+                ligne_region = Regions(
+                    region = entry.get('nom_region')
+                )
+                session.add(ligne_region)
+                ligne_formation_ext.region.append(ligne_region)
+
+            elif region_exist and entry.get('nom_region'):
+                ligne_formation_ext.region.append(region_exist)
+
         session.commit()
     except:
         session.rollback()
